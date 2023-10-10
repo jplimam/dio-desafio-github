e0153ca1b1beaaac245d3e11720ccaa1c5719ba7
jplimam <limamarcelojoaopedro@gmail.com>
2023-10-10 01:17:47 -0300

    Aulas 3, 4, 5 de sintaxe básica de Javascript.

---
+++ javascript-aulas/sintaxe-javascript/aula-5/script.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log('Essa mensagem veio de uma função.');
+}
+
+funcao();
+
+function funcaoParametro (primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+funcaoParametro('Tudo certo', 'jovem!');
