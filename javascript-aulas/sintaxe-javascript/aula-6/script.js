d033b48f47f46ddb55d58c9225f2fa1ce0027bae
jplimam <limamarcelojoaopedro@gmail.com>
2023-10-10 10:18:58 -0300

    Aula 6, avançando em funções e criação de calculadora utilizando Javascript.

---
+++ javascript-aulas/sintaxe-javascript/aula-6/script.js
@@ -0,0 +1,26 @@
+// tipos de função
+
+/* declarativas
+function funcao() {
+    console.log('Sou uma mensagem de uma função declarativa')
+}
+
+funcao();
+
+
+// expressão de função
+// com nomeação
+var funcao = function funcao() {
+    console.log('Essa mensagem vem de uma função de expressão!')
+}
+
+funcao();
+
+
+// arrow function - anônimas, não nomeadas
+var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+
+funcao();
+*/
